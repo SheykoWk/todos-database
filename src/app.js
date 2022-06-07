484c0577df1db3a0a1dc9663034ad6557f722a37
sheyko <sheyko.wk@gmail.com>
2022-06-06 19:58:11 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const config = require('../config')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!')
+})
+
+
+app.listen(config.port, () => {
+    console.log(`Server started at port ${config.port}`)
+})
+
+
+
+
+
+
+
