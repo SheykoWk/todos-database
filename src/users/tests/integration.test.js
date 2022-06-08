f94e7a5466984c0f3f521df68f6387ff17ed23ef
sheyko <sheyko.wk@gmail.com>
2022-06-07 20:53:22 -0500

    add integration test to users

---
+++ src/users/tests/integration.test.js
@@ -0,0 +1,41 @@
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../../app').app
+
+chai.use(chaiHttp)
+
+const usersDB = [
+    {
+        id: 1,
+        first_name: "Sahid",
+        last_name: "Kick",
+        email: "sahid.kick@academlo.com",
+        password: "1234",
+        active: true
+    },
+    {
+        id: 2,
+        first_name: "Juan",
+        last_name: "Alvarez",
+        email: "juanito@gmail.com",
+        password: "4321",
+        active: false
+    }
+]
+
+describe('Integration Testing For Users', () => {
+    it('Should return all the users', (done) => {
+        chai.request(app)
+            .get('/api/v1/users')
+            .end((err, res) => {
+                console.log(res)
+                chai.assert.equal(res.body, usersDB)
+                chai.assert.equal(res.status, 200)
+                chai.assert.equal(res.body.length, usersDB.length)
+            })
+    })
+})
+
+
+
+
--- src/users/users.routes.js
+++ src/users/users.routes.js
@@ -1,10 +1,28 @@
 const router = require('express').Router();
 
+const usersDB = [
+    {
+        id: 1,
+        first_name: "Sahid",
+        last_name: "Kick",
+        email: "sahid.kick@academlo.com",
+        password: "1234",
+        active: true
+    },
+    {
+        id: 2,
+        first_name: "Juan",
+        last_name: "Alvarez",
+        email: "juanito@gmail.com",
+        password: "4321",
+        active: false
+    }
+]
+
+
 router.route('/users')
     .get((req, res) => {
-        res.json({
-            message: "Hola"
-        })
+        res.status(200).json(usersDB)
     })
     .post()
 
